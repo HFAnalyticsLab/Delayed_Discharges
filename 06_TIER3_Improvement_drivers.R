ef204ca88e6ddf3c83dd09baf713a5854c7fc6d7
George Stevenson <george.stevenson@health.org.uk>
2025-08-29 11:20:23 +0100

    Average length of delay

--- 06_TIER3_Improvement_drivers.R
+++ 06_TIER3_Improvement_drivers.R
@@ -3,6 +3,11 @@
 
 # Drivers of DD improvement ###################################################
 
+# Greater than 2.5% reduction (10 trusts)
+greater_than_2.5p_reduction <- figure_12_data %>%
+  select(org_code,difference) %>%
+  filter(difference <= -2.5)
+
 best_trusts <- c(greater_than_2.5p_reduction$org_code)
 
 # 1 Bed occupancy #############################################################
@@ -11,7 +16,7 @@ best_trusts <- c(greater_than_2.5p_reduction$org_code)
 
 figure_3.1_data <- hospital_beds %>%
   filter(org_code %in% best_trusts) %>%
-  mutate(month = my(month))
+  mutate(month = my(month)) %>%
   group_by(org_code)
   
 bed_occupancy_plot <- ggplot(figure_3.1_data, aes(x = month, y = occupancy_rate, group = org_code, color = org_code)) +
@@ -36,6 +41,40 @@ bed_occupancy_plot
 ggsave("bed_occupancy_plot.png", plot = bed_occupancy_plot, width = 8, height = 6, dpi = 300)
 
 
+# 2 Delay length by trust
+
+figure_3.2_data <- dd_file_acute_trusts_FINAL %>%
+  filter(org_code %in% best_trusts) %>%
+  select(month,org_code,average_delay_los_minus_0_day_delay) %>%
+  mutate(month = my(month)) %>%
+  group_by(org_code)
+
+average_trust_delay_plot <- ggplot(figure_3.2_data, aes(x = month, y = average_delay_los_minus_0_day_delay, group = org_code, color = org_code)) +
+  geom_line(linewidth = 1.2) +
+  geom_point(color = "black", size = 1.0) +
+  stat_summary(
+    aes(group = 1),       
+    fun = mean, 
+    geom = "line", 
+    colour = "black", 
+    linewidth = 1.5, 
+    linetype = "22") +
+  labs(title = "Average length of delay amongst best performing trusts April 2024 - June 2025.",
+       x = "Month",
+       y = "Average days from DRD to DoD (exc 0-day)") +
+  scale_y_continuous(
+    limits = c(0, 15),        
+    breaks = seq(0, 15, 1)) +
+  scale_x_date(
+    date_labels = "%b-%y",
+    date_breaks = "1 month") +
+  theme_gray() +
+  theme(axis.text.x = element_text(size = 6.5)) +
+  theme(axis.title.x = element_text(margin = margin(t = 12)),
+        axis.title.y = element_text(margin = margin(r = 12)))
+
+average_trust_delay_plot
+ggsave("average_trust_delay_plot.png", plot = average_trust_delay_plot, width = 8, height = 6, dpi = 300)
 
 
 
